4a967080d56b501ed77fa730e48a69a24cb77f5d
Nahom Assefa <nzenebe@gmail.com>
2022-04-20 12:21:09 -0500

    okay coo

---
+++ VariableTypes.java
@@ -0,0 +1,13 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        int age = 10;
+        double myAge = 10.99;
+        String three = "dog";
+        double sum = age + myAge;
+
+        String yes = three + age;
+
+        System.out.println(sum);
+        System.out.println(yes);
+    }
+}
